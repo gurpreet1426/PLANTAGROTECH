d9dd9c24dc12de8e53fee9e001de6b869365c506
gurpreet1426 <gurpreetaws@gmail.com>
2025-06-04 21:22:53 +0530

    second commit to test pipeline

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 app.get('/api/get', (req,res)=>{
     res.send({message:'Node JS api deployement'})
 })
+app.get('/api/get/user', (req,res)=>{
+    res.send({name:'Dev', email:"gurpreet", 'role':'admin'})
+})
 app.listen(9000,(req,res)=>{
     console.log('server running on port 9000')
 })
