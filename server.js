1d4c0ccc13a521e8463908b5904d0c1b6b8e8373
gurpreet1426 <gurpreetaws@gmail.com>
2025-06-28 23:04:28 +0530

    code to add more endpoints

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const app = express();
-const sequelize = require('../PLANTAGROTECH/index');
+//const sequelize = require('../PLANTAGROTECH/index');
 // simple route
-app.get('/api/get', async (req, res) => {
- try {
-    await sequelize.authenticate();
-    res.json({ message: 'Connection has been established successfully.' });
-  } catch (error) {
-    res.status(500).json({ message: 'Unable to connect to the database.', error });
-  }
+// app.get('/api/get', async (req, res) => {
+//  try {
+//     await sequelize.authenticate();
+//     res.json({ message: 'Connection has been established successfully.' });
+//   } catch (error) {
+//     res.status(500).json({ message: 'Unable to connect to the database.', error });
+//   }
+// });
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Plant Agro tech application." });
 });
+const router = require('./app/routes/router');
+app.use('/', router);
 
 const PORT = 9000;
 app.listen(PORT, (req,res) => {
