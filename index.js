80388b4e4f4783441176ad5681d187f372426006
gurpreet1426 <gurpreetaws@gmail.com>
2025-06-28 22:58:12 +0530

    code to connect with postgress

---
+++ index.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+const dbConfig= require("../PLANTAGROTECH/config/db.config.js");
+//const dbConfig2 = require("../../config/db.config.js");
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+  host: dbConfig.HOST,
+  dialect: dbConfig.dialect,
+
+  pool: {
+    max: dbConfig.pool.max,
+    min: dbConfig.pool.min,
+    acquire: dbConfig.pool.acquire,
+    idle: dbConfig.pool.idle
+  }
+});
+
+module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
-
+const sequelize = require('../PLANTAGROTECH/index');
 // simple route
-app.get('/api/get', (req, res) => {
-  res.send({ message: "Welcome to Plant Agro tech application." });
+app.get('/api/get', async (req, res) => {
+ try {
+    await sequelize.authenticate();
+    res.json({ message: 'Connection has been established successfully.' });
+  } catch (error) {
+    res.status(500).json({ message: 'Unable to connect to the database.', error });
+  }
 });
 
 const PORT = 9000;
