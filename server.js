c36779472cf1cb1abcec8adc1bac842c755a47d1
gurpreet1426 <gurpreetaws@gmail.com>
2025-06-28 14:13:00 +0530

    code refactor

--- server.js
+++ server.js
@@ -8,11 +8,8 @@ app.use(express.json());
 app.get("/welcome", (req, res) => {
   res.json({ message: "Welcome to Plant Agro tech application." });
 });
-// const router = require('./app/routes/router');
-// app.use('/', router);
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
