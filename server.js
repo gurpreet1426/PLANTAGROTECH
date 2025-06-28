54f93c36098908ce99473642225c3a606c62f965
gurpreet1426 <gurpreetaws@gmail.com>
2025-06-28 22:21:03 +0530

    Roll back all the changes

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const app = express();
-// parse requests of content-type - application/json
-app.use(express.json());
-
 
 // simple route
-app.get("/welcome", (req, res) => {
-  res.json({ message: "Welcome to Plant Agro tech application." });
+app.get('/api/get', (req, res) => {
+  res.send({ message: "Welcome to Plant Agro tech application." });
 });
 
 const PORT = 9000;
-app.listen(PORT, () => {
+app.listen(PORT, (req,res) => {
   console.log(`Server is running on port ${PORT}.`);
 });
