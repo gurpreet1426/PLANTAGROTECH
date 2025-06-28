92aaebeff54ec3e1bfd893bc04439da48b6ce253
gurpreet1426 <gurpreetaws@gmail.com>
2025-06-28 22:48:39 +0530

    code to configure database

---
+++ config/db.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  // HOST: "localhost",
+  // USER: "postgres",
+  // PASSWORD: "12345",
+  // DB: "plantagrotech",
+  // dialect: "postgres",
+  // pool: {
+  //   max: 5,
+  //   min: 0,
+  //   acquire: 30000,
+  //   idle: 10000
+  // }
+
+  HOST: "database-1.cdw06eysqxxo.ap-south-1.rds.amazonaws.com",
+  USER: "postgres",
+  PASSWORD: "Japkaran2025",
+  DB: "plantagrotech",
+  dialect: "postgres",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
