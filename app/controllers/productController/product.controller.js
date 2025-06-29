adcefd93f8a3f10dde590d73a1643431225beb95
gurpreet1426 <gurpreetaws@gmail.com>
2025-06-29 13:55:04 +0530

    code for another endpoints

--- app/controllers/productController/product.controller.js
+++ app/controllers/productController/product.controller.js
@@ -3,7 +3,7 @@ const express= require ('express');
 const router = express.Router();
 
 // Create a new Product
-//router.post("/", products.create);
+router.post("/", products.create);
 
 // Retrieve all Products
 router.get("/", products.findAll);
@@ -11,13 +11,13 @@ router.get("/", products.findAll);
 router.get("/dummy", products.dummy);
 
 // Retrieve all published Products
-//router.get("/published", products.findAllPublished);
+router.get("/published", products.findAllPublished);
 
 // Retrieve a single Product with id
-//router.get("/:id", products.findOne);
+router.get("/:id", products.findOne);
 
 // Update a Product with id
-//router.put("/:id", products.update);
+router.put("/:id", products.update);
 
 // Delete a Product with id
 //router.delete("/:id", products.delete);
